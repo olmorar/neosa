12df3c9560a1d3f10aa0ad1541c25d0800b05dcd
olmorar <olexandr.morar@gmail.com>
2025-02-09 01:17:07 +0200

    Refactor Navbar component and update authentication to use GitHub provider

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,9 @@
 import { ReactNode } from "react";
-import { Navbar } from "@/app/components";
+import { Navbar } from "@/app/ui";
 
 export default function Layout({
   children,
-}: Readonly<{ children: ReactNode }>) {
+}: Readonly<{ children: ReactNode; }>) {
   return (
     <main className="font-work-sans">
       <Navbar />
